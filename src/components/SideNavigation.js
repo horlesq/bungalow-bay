7ee77ae2a90c3440a4c37121d21157b79c6a0ccf
DESKTOP-VQ917G5\Horlesq <adrian.horlescu@gmail.com>
2025-09-09 11:02:30 +0300

    heighlight current guest tab

--- src/components/SideNavigation.js
+++ src/components/SideNavigation.js
@@ -1,5 +1,9 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import SignOutButton from "./SignOutButton";
 import { Calendar, Home, User } from "lucide-react";
+import Link from "next/link";
 
 const navLinks = [
     {
@@ -20,18 +24,24 @@ const navLinks = [
 ];
 
 function SideNavigation() {
+    const pathname = usePathname();
+
     return (
         <nav className="border-r border-primary-900">
             <ul className="flex flex-col gap-2 h-full text-lg">
                 {navLinks.map((link) => (
                     <li key={link.name}>
-                        <a
-                            className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200`}
+                        <Link
+                            className={`${
+                                pathname === link.href
+                                    ? "bg-primary-900 text-primary-100"
+                                    : ""
+                            } py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200`}
                             href={link.href}
                         >
                             {link.icon}
                             <span>{link.name}</span>
-                        </a>
+                        </Link>
                     </li>
                 ))}
 
