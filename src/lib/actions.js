b51f31dafb70058d6fdbd5fb54d088cb6da19566
DESKTOP-VQ917G5\Horlesq <adrian.horlescu@gmail.com>
2025-09-24 09:51:44 +0300

    edit booking - in progress

--- src/lib/actions.js
+++ src/lib/actions.js
@@ -61,3 +61,63 @@ export async function deleteBookingAction(bookingId) {
 
     revalidatePath("/account/reservations");
 }
+
+/** Update booking action */
+export async function deleteBooking(bookingId) {
+    const session = await auth();
+    if (!session) throw new Error("You must be logged in");
+
+    const guestBookings = await getBookings(session.user.guestId);
+    const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+    if (!guestBookingIds.includes(bookingId))
+        throw new Error("You are not allowed to delete this booking");
+
+    const { error } = await supabase
+        .from("bookings")
+        .delete()
+        .eq("id", bookingId);
+
+    if (error) throw new Error("Booking could not be deleted");
+
+    revalidatePath("/account/reservations");
+}
+
+export async function updateBookingAction(formData) {
+    const bookingId = Number(formData.get("bookingId"));
+
+    // 1) Authentication
+    const session = await auth();
+    if (!session) throw new Error("You must be logged in");
+
+    // 2) Authorization
+    const guestBookings = await getBookings(session.user.guestId);
+    const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+    if (!guestBookingIds.includes(bookingId))
+        throw new Error("You are not allowed to update this booking");
+
+    // 3) Building update data
+    const updateData = {
+        numGuests: Number(formData.get("numGuests")),
+        observations: formData.get("observations").slice(0, 1000),
+    };
+
+    // 4) Mutation
+    const { error } = await supabase
+        .from("bookings")
+        .update(updateData)
+        .eq("id", bookingId)
+        .select()
+        .single();
+
+    // 5) Error handling
+    if (error) throw new Error("Booking could not be updated");
+
+    // 6) Revalidation
+    revalidatePath(`/account/reservations/edit/${bookingId}`);
+    revalidatePath("/account/reservations");
+
+    // 7) Redirecting
+    redirect("/account/reservations");
+}
